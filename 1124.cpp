e90e7221d1c9f118c680c2f3f84354bbd8b2bd1d
Satyarth Agrahari <satyarthagrahari@gmail.com>
2017-01-24 19:28:33 +0530

    Create 1124.cpp

---
+++ 1124.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mkp make_pair
+
+int main()
+{
+	char c;
+
+	while ((c = getchar()) != EOF) {
+		cout << c;
+	}
+
+	return 0;
+}
